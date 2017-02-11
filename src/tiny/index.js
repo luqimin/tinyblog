246f8e8162ef431e8df1377845fdea225d895be3
QiminLU <qilu@163.com>
2017-02-11 13:17:03 +0800

    添加pm2设置，修改静态文件路径

--- src/tiny/index.js
+++ src/tiny/index.js
@@ -9,7 +9,11 @@ const app = new Koa();
 // log request URL:
 app.use(async(ctx, next) => {
     console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
+    let start = new Date().getTime(),
+        execTime;
     await next();
+    execTime = new Date().getTime() - start;
+    ctx.response.set('X-Response-Time', `${execTime}ms`);
 });
 
 app.use(staticServer(CONFIG.staticDir));
